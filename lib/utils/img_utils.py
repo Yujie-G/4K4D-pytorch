2be8d3c11df3002fc346c8744a23adc51fab3c7c
yujie <yujieguo0012@gmail.com>
2024-05-30 14:24:42 +0800

    add related utils from source code

--- lib/utils/img_utils.py
+++ lib/utils/img_utils.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 def save_tensor_image(image_tensor, file_path):
     # Convert the tensor to a numpy array
-    image_np = image_tensor.cpu().numpy()
+    image_np = image_tensor.detach().cpu().numpy()
 
     # Convert the numpy array to a PIL Image
     image_pil = Image.fromarray(np.uint8(image_np))
@@ -19,6 +19,12 @@ def save_tensor_image(image_tensor, file_path):
     # Save the PIL Image to the specified file path
     image_pil.save(file_path)
 
+def save_numpy_image(image_np, file_path):
+    # Convert the numpy array to a PIL Image
+    image_pil = Image.fromarray(np.uint8(image_np))
+
+    # Save the PIL Image to the specified file path
+    image_pil.save(file_path)
 
 def unnormalize_img(img, mean, std):
     """
